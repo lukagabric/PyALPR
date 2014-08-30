1d1bb10cb960ef1ac61af5b062d528dba79939be
Luka Gabric <luka.gabric@gmail.com>
2014-08-30 11:03:50 +0200

    PyALPR

--- PyALPR.py
+++ PyALPR.py
@@ -4,7 +4,7 @@ if __name__=="__main__":
     webcam_command = "fswebcam -r 640x480 -S 20 --no-banner alpr.jpg"
     webcam_command_args = shlex.split(webcam_command)
 
-    webcam_proc = subprocess.Popen(alpr_command_args, stdout=subprocess.PIPE)
+    webcam_proc = subprocess.Popen(webcam_command_args, stdout=subprocess.PIPE)
     (webcam_out, webcam_err) = webcam_proc.communicate()
 
     print "webcam output:", webcam_out
