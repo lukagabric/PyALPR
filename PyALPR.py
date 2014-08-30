cecb3081e067bd669b51db2e311808f64afa4fbc
Luka Gabric <luka.gabric@gmail.com>
2014-08-30 15:31:17 +0200

    PyALPR

--- PyALPR.py
+++ PyALPR.py
@@ -1,17 +1,27 @@
-import os, shlex, subprocess
+import json, shlex, subprocess
 
 if __name__=="__main__":
     webcam_command = "fswebcam -r 640x480 -S 20 --no-banner --quiet alpr.jpg"
     webcam_command_args = shlex.split(webcam_command)
 
     webcam_proc = subprocess.Popen(webcam_command_args, stdout=subprocess.PIPE)
-    (webcam_out, webcam_err) = webcam_proc.communicate()
-
-    print "webcam output:", webcam_out
+    webcam_proc.communicate()
 
     alpr_command = "alpr -c eu -t hr -n 300 -j alpr.jpg"
     alpr_command_args = shlex.split(alpr_command)
 
     alpr_proc = subprocess.Popen(alpr_command_args, stdout=subprocess.PIPE)
     (alpr_out, alpr_err) = alpr_proc.communicate()
-    print "alpr output:", alpr_out
+
+    alpr_json = json.loads(alpr_out)
+    results = alpr_json["results"]
+
+    order = 0
+    for result in results:
+        matches_template = result["matches_template"]
+        if matches_template == 1:
+            plate = result["plate"]
+            order += 1
+            print "Plate {0:d}.: %s".format(order, plate)
+
+    print "------------"
