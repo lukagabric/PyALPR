10ed0c27b0e5c7aeb2cb2f36eb83d7c871b27bc1
Luka Gabric <luka.gabric@gmail.com>
2014-08-30 11:03:22 +0200

    PyALPR

--- PyALPR.py
+++ PyALPR.py
@@ -2,11 +2,16 @@ import os, shlex, subprocess
 
 if __name__=="__main__":
     webcam_command = "fswebcam -r 640x480 -S 20 --no-banner alpr.jpg"
-    os.system(webcam_command)
+    webcam_command_args = shlex.split(webcam_command)
+
+    webcam_proc = subprocess.Popen(alpr_command_args, stdout=subprocess.PIPE)
+    (webcam_out, webcam_err) = webcam_proc.communicate()
+
+    print "webcam output:", webcam_out
 
     alpr_command = "alpr -c eu -t hr -n 300 -j alpr.jpg"
     alpr_command_args = shlex.split(alpr_command)
 
     alpr_proc = subprocess.Popen(alpr_command_args, stdout=subprocess.PIPE)
-    (out, err) = alpr_proc.communicate()
-    print "program output:", out
+    (alpr_out, alpr_err) = alpr_proc.communicate()
+    print "alpr output:", alpr_out
